79922f9bc2e949f4def23b6b254c406c961f1780
siam <siamahamedab@gmail.com>
2026-04-10 20:03:46 +0600

    Create course service

--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CourseService {}
+export class CourseService {
+    getAllCourse(): string {
+        return 'Get all course -from service';
+    }
+    getCourseByID(id:string): string{
+        return 'Get course with ID: ${id} -from service';
+    }
+    createCourse():string{
+        return 'create course -from service';
+    }
+    updateCourse(id:string):string {
+        return 'Update coruse ${id} -from service';
+    }
+    patchCourse(id:string):string{
+        return 'Patch Course ${id} -from service'
+    }
+    deleteCourse(id:string):string{
+        return 'Delete Course ${id} -from service';
+    }
+}
